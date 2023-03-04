cbd5ab5501c422ecc4e104c35db5cc2a7a459897
vegeterianlee <max322318@gmail.com>
2023-03-04 22:51:33 +0900

    fix(websecurity,commentcontroller):cors active

--- src/main/java/com/sparta/sweetterbe/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/sweetterbe/config/WebSecurityConfig.java
@@ -79,13 +79,12 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**") // 프로그램에서 제공하는 URL
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://api.jingyulee.shop/docs")
-                .allowedOrigins("http://api.jingyulee.shop/docs")// 요청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
+                .allowedOrigins("*")
+                //.allowedOrigins("http://localhost:3000","https://api.jingyulee.shop/docs","http://api.jingyulee.shop/docs")// 요청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
                 .allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST...)
                 .allowedHeaders("*", "Content-Type") // 어떤 헤더들을 허용할 것인지
                 .exposedHeaders("Authorization")
-                .allowCredentials(true) // 쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
+                .allowCredentials(false) // 쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
                 // origin값에 주소를 구체적으로 명시해야한다
                 .maxAge((long)3600 * 24 * 365); // preflight 요청에 대한 응답을 브라우저에서 캐싱하는 시간;
     }
