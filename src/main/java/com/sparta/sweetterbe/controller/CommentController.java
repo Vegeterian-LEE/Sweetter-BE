2f52beea3c89747a600412661ffbdff1d61a7f09
vegeterianlee <max322318@gmail.com>
2023-03-04 18:57:05 +0900

    fix(websecurity,commentcontroller):cors unactive

--- src/main/java/com/sparta/sweetterbe/controller/CommentController.java
+++ src/main/java/com/sparta/sweetterbe/controller/CommentController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.naming.AuthenticationException;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/comment")
