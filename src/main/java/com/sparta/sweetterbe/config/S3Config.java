2f9d1faa120b07f4dc774869a5d774e87e027bab
vegeterianlee <max322318@gmail.com>
2023-03-04 18:20:37 +0900

    null

--- src/main/java/com/sparta/sweetterbe/config/S3Config.java
+++ src/main/java/com/sparta/sweetterbe/config/S3Config.java
@@ -23,5 +23,6 @@ public class S3Config {
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
-                .build();}
+                .build();
+    }
 }
