b0e0d5d1067721bf733b24ac368dd0d936a5eb1e
vegeterianlee <max322318@gmail.com>
2023-03-05 05:50:19 +0900

    null

--- src/main/java/com/sparta/sweetterbe/controller/FollowController.java
+++ src/main/java/com/sparta/sweetterbe/controller/FollowController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
 import java.nio.file.AccessDeniedException;
 
 @RestController
