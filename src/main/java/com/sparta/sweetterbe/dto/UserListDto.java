7bf39f3c359f19ed8390e6b125b048c39a47fa87
the1224 <asdz2003@gmail.com>
2023-03-05 15:15:20 +0900

    feat : background image 관련 기능 추가

--- src/main/java/com/sparta/sweetterbe/dto/UserListDto.java
+++ src/main/java/com/sparta/sweetterbe/dto/UserListDto.java
@@ -13,13 +13,13 @@ public class UserListDto {
     private String UserId;
     private String username;
     private String introduction;
-    private String image;
+    private String profileImage;
     private boolean followed;
     public UserListDto(User user, boolean followed){
         this.UserId = user.getUserId();
         this.username = user.getUsername();
         this.introduction = user.getIntroduction();
-        this.image = user.getImage();
+        this.profileImage = user.getProfileImage();
         this.followed = followed;
     }
 
