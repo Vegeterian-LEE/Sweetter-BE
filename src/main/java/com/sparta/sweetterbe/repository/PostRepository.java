bf6e6037a612286f63e1400e906cfcae47069d13
vegeterianlee <max322318@gmail.com>
2023-03-08 13:30:40 +0900

    fix:프로필 tweetlist(내림차순 정렬과 본인 게시글의 retweetcheck==null 수정)

--- src/main/java/com/sparta/sweetterbe/repository/PostRepository.java
+++ src/main/java/com/sparta/sweetterbe/repository/PostRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findAllByIdOrderByCreatedAtDesc(Long id);
+    List<Post> findAllByUserIdOrderByCreatedAtDesc(Long userid);
 
     List<Post> findAllByUserOrderByCreatedAtDesc(User user);
 
